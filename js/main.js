764c8ff18806501f923ee97a44f4df0aaf2fd63a
Braydon Powell <thetic1@gmail.com>
2016-03-28 10:43:01 -0600

    Went through the tutorial with landon in class

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 //This is where the JS magig happens
 
   // Add Ref to Firebase
+  var messagesRef = new Firebase("https://chatbp.firebaseio.com/");
 
   // Register the Dom Elaments with jQuery
   var messageField = $('#messageInput');
@@ -10,18 +11,42 @@
   // Listem for Enter press
   messageField.keypress(function (e) {
     if (e.keyCode == 13) {
+        console.log("Pressing Enter");
       //save data to firebase and clear the message field
+      
+      var name = nameField.val();
+      var message = messageField.val();
+        
+      messagesRef.push({name: name, text: message});
+        
+      messageField.val('');
 
     }
   });
 
   // Add a callback that is triggered for each chat message.
+    messagesRef.on("child_added", function(snapshot, prevChildKey){
+        
+        // get the data
+        var newMessage = snapshot.val();
+        
+        var username = newMessage.name;
+        var message = newMessage.text;
+        
+        console.log(newMessage);
+        
+        // Create Elements for Message (<li> for messageElement and <strong class='example-chat-username'></strong> for nameElement)
 
-      // get the data
-
-      // Create Elements for Message (<li> for messageElement and <strong class='example-chat-username'></strong> for nameElement)
-
-      // Add the message to messageList
-
-      //Scroll to bottom of MessageList
-
+        var messageElement = $("<li>");
+        var nameElement = $("<li> for messageElement and <strong class='example-chat-username'></strong>");
+        
+        nameElement.text(username);
+        messageElement.text(message).prepend(nameElement);
+        
+        // Add the message to messageList
+        messageList.append(messageElement);
+        
+        //Scroll to bottom of MessageList
+        messageList[0].scrollTop = messageList[0].scrollHeight;
+        
+    });
